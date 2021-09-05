c38e799d7722c149957bb69d5b6a3b1dcc5e7e04
fuguijar <fuguijar@qq.com>
2021-09-05 16:27:35 +0800

    2021-09-05

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+   * @author fuguijar.top
+   * index.js
+   * -v 1.0
+   *  超简UI
+   * 
+**/
+
+var link = document.createElement('link');
+link.rel = "stylesheet";
+link.href = "lib/core.css";
+document.getElementsByTagName("head")[0].appendChild(link);
